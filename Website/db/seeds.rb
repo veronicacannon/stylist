a2c6b0336e02cf617215603858e2fd48be1ff564
Sue-Ting Chene <sueting@gmail.com>
2014-04-22 11:26:56 -0700

    create admin user using rake db:seed instead going in rails console

--- Website/db/seeds.rb
+++ Website/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(full_name: 'Admin user', username: 'admin', password: 'password')
