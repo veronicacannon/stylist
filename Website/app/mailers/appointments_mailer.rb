108a6e095e1a6677a22fe3ecbd1eca8815f17e8a
veronicacannon <me@veronicacannon.com>
2014-04-26 17:48:13 -0700

    Refactor appointments.mailer

--- Website/app/mailers/appointments_mailer.rb
+++ Website/app/mailers/appointments_mailer.rb
@@ -1,26 +1,34 @@
 class AppointmentsMailer < ActionMailer::Base
-  include Icalendar #imports Event object
-  
+
   default from: Application.config.from_email_address,
           to: Application.config.to_email_address,
-          subject: "New appointment"
+          subject: "Hair Appointment"
   
-  def new_appointment_stylist(appointment)
+  def new_appointment_to_stylist(appointment)
     @appmnt = appointment
-    ics_attachment = StringIO.new(create_ics().to_ical)
-    attachments['appointment.ics'] = ics_attachment.read
-    ics_attachment.close
-    mail()
+
+    StringIO.open(create_ics.to_ical) do |ics|
+      attachments['appointment.ics'] = ics.read
+    end
+
+    mail
   end
   
   private
-  def create_ics()
-    event = Event.new
-    event.start = @appmnt.date
-    event.summary = 'Name: ' + @appmnt.firstname + ' ' + @appmnt.lastname '\n'\
-                    'Date: ' + @appmnt.date + '\n'\
-                    'Service: ' + @appmnt.service + '\n'\
-                    'Email: ' + @appment.email
-    return event
+
+  def create_ics
+    Icalendar::Event.new.tap do |event|
+      event.start   = @appmnt.date
+      event.summary = summary
+    end
+  end
+
+  def summary
+    String.new.tap do |summary|
+      summary << "Name: #{@appmnt.firstname} #{@appmnt.lastname} \n"
+      summary << "Date: #{@appmnt.date} \n"
+      summary << "Service: #{@appmnt.service} \n"
+      summary << "Email: #{@appment.email}"
+    end
   end
 end
