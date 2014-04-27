e0a02204f080c9293614d9c5066a4321c385ba61
Seberius <admin@infiniteskycorp.com>
2014-04-26 19:01:31 -0700

    Merged client mailer method into appointments_mailer.rb and renamed to appointment_mailer.rb. Also updated to use correct Appointment class attr names. Mailer templates renamed to new Appointment to_client method name and duplicated for to_stylist method, along with removing unused templates. The to_stylist method was changed to use config.stylist_email_address for delivery address.

--- Website/app/mailers/appointment_mailer.rb
+++ Website/app/mailers/appointment_mailer.rb
@@ -1,10 +1,40 @@
 class AppointmentMailer < ActionMailer::Base
-  default from: "haircut@ltol.com"
 
-  def appointment_email(appointment)
+  default from: Application.config.from_email_address,
+          subject: "Hair Appointment with #{Application.config.stylist_name}",
+  
+  def new_appointment_to_stylist(appointment)
     @appointment = appointment
-    mail(to: 'me@veronicacannon.com', 
-    	subject: 'Hair Appointment with Alicia',
-    	context_type: 'text/html')
-end
+
+    StringIO.open(create_ics.to_ical) do |ics|
+      attachments['appointment.ics'] = ics.read
+    end
+
+    mail(to: Application.config.stylist_email_address)
+  end
+  
+  def new_appointment_to_client(appointment)
+    @appointment = appointment
+    
+    mail(to: @appointment.email)
+  end
+  
+  private
+
+  def create_ics
+    Icalendar::Event.new.tap do |event|
+      event.start   = @appointment.appot
+      event.summary = summary
+    end
+  end
+
+  def summary
+    String.new.tap do |summary|
+      summary << "Name: #{@appointment.first_name} #{@appointment.last_name} \n"
+      summary << "Date: #{@appointment.appot} \n"
+      summary << "Service: #{@appointment.service} \n"
+      summary << "Email: #{@appointment.email}"
+      summary << "Phone: #{@appointment.phone}"
+    end
+  end
 end
