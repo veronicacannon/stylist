ebe4d194c10d78f61977288e1ff43246d03fbc6a
Seberius <admin@infiniteskycorp.com>
2014-04-27 22:56:09 -0700

    Added the Letter Opener gem for the dev enviroment.

--- Website/config/environments/development.rb
+++ Website/config/environments/development.rb
@@ -43,7 +43,7 @@ Website::Application.configure do
   #config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.smtp_settings = {
   address:              'mail.ltol.com',
   port:                 587,
